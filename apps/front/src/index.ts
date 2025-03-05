f2bf81b38f1e47465ac0c89abd8d6ddf4b3020cc
Eiren Rain <eirenliel@noreply.github.com>
2025-03-05 19:39:00 +0100

    basic text on screen

--- apps/front/src/index.ts
+++ apps/front/src/index.ts
@@ -10,17 +10,32 @@ const {
 
 const {
   onMessage,
+
 } = createWebsocketClient()
 
+let currentAngle = { x: 0, y: 0, z: 0 }
+
 onMessage('server/gyro/angles', ({ angles }) => {
-  // console.log('angles', angles)
+  currentAngle = angles;
   setRotation(angles)
+
+  // You can also do it here
+  const angleText = document.getElementById("angle-text")!; // <-- whath out for the ! at the end 
+  angleText.innerHTML = `Current angle x: ${angles.x} y: ${angles.y} z: ${angles.z}`;
 })
 
 const animate = () => {
   renderer.render(scene, camera);
   requestAnimationFrame(animate);
 };
+
+
+setInterval(() => { // This is to prevent to re render every threejs -> do not update the dom in animate!
+  const angleText = document.getElementById("angle-text")!;
+
+  angleText.innerHTML = `Current angle x: ${currentAngle.x} y: ${currentAngle.y} z: ${currentAngle.z}`;
+}, 100) // this will run every 100ms
+
 
 animate();
 
